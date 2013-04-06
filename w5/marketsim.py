ffddab2e1d2f30eec3635f536846ea9985bea0b2
baio <max.putilov@gmail.com>
2013-04-06 18:34:31 +0400

    marketsim final

--- w5/marketsim.py
+++ w5/marketsim.py
@@ -19,34 +19,33 @@ if __name__ == '__main__':
                 "is_buy": t[4] == "Buy", "amount": int(t[5])} for t in na_data]
 
     ls_symbols = set([t["symbol"] for t in na_data])
-    ldt_dates = [t["date"] for t in na_data]
+    ldt_dates = sorted([t["date"] for t in na_data])
     dt_min = min(ldt_dates)
     dt_max = max(ldt_dates) + dt.timedelta(days=1)
 
     ldt_timestamps = du.getNYSEdays(dt_min, dt_max, dt.timedelta(hours=16))
 
     dataobj = da.DataAccess('Yahoo')
-    ls_keys = ['close', 'actual_close']
+    ls_keys = ['close', 'close']
     ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
 
-    dd_own = copy.deepcopy(d_data['actual_close'])
+    dd_own = copy.deepcopy(d_data['close'])
     dd_own = dd_own * 0
     ld_cash = dict()
     perv_owns = dict()
     perv_val = d_value
 
     for timestamp in ldt_timestamps:
-        date = timestamp.date()
+        "next date actually"
         "update current owns accordingly to pervious owns"
-        #ld_cash[timestamp] = perv_val
         for symbol in perv_owns.keys():
             dd_own[symbol][timestamp] = perv_owns[symbol]
-        found_orders = [t for t in na_data if t["date"].date() == date]
+        found_orders = [t for t in na_data if (t["date"] + dt.timedelta(hours=16)) == timestamp]
         for order in found_orders:
             symbol = order["symbol"]
             amt = order["amount"]
-            d_actual_close = d_data["actual_close"][symbol]
+            d_actual_close = d_data["close"][symbol]
             price_actual = d_actual_close[timestamp]
             d_order_value = price_actual * amt
             if order["is_buy"]:
@@ -58,11 +57,10 @@ if __name__ == '__main__':
             perv_owns[symbol] = dd_own[symbol][timestamp]
         #perv_val = d_value
         ld_cash[timestamp] = d_value
+        #print timestamp, d_value
 
     print dd_own.head(10)
     print dd_own.tail(10)
-
-
 
     with open(ls_values_file, "w+") as f_out:
         for timestamp in ldt_timestamps:
@@ -70,7 +68,7 @@ if __name__ == '__main__':
             value = 0
             for symbol in ls_symbols:
                 own = dd_own[symbol][timestamp]
-                value +=  d_data["actual_close"][symbol][timestamp] * own
+                value +=  d_data["close"][symbol][timestamp] * own
             total = cash + value
             f_out.write("{0}, {1}, {2}, {3}\n".format(timestamp.year, timestamp.month, timestamp.day, int(total)))
 
