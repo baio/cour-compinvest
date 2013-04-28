a8655d27ebfc370fbb22b85b5d4d6b62f7923950
baio <max.putilov@gmail.com>
2013-04-28 21:43:44 +0400

    w8

--- CompInvest/events_gens/bollinger_minus_2_spy_1.py
+++ CompInvest/events_gens/bollinger_minus_2_spy_1.py
@@ -11,18 +11,42 @@ def find_events(d_data, ndays=20):
     vals = bollinger_df(df_close, ndays)
     vals_merged = pd.merge(vals, vals.shift(1), left_index=True, right_index=True)
 
-    """
-    print vals_merged[(vals_merged["SPY.x"] >= 1)].head()
-    print vals_merged[(vals_merged["%s.x" % "NU"] <= -2) & (vals_merged["%s.y" % "NU"] >= -2)].head()
-    print vals_merged[(vals_merged["%s.x" % "NU"] <= -2) & (vals_merged["%s.y" % "NU"] >= -2) & (vals_merged["SPY.x"] >= 1)].head()
-    """
-
     # Creating an empty dataframe
     df_events = copy.deepcopy(df_close)
     df_events = df_events * np.NAN
 
     for symbol in vals.columns:
         df_events.ix[(vals_merged["%s.x" % symbol] <= -2) & (vals_merged["%s.y" % symbol] >= -2) & (vals_merged["SPY.x"] >= 1), symbol] = 1
-        #print df_events[(vals_merged["%s.x" % symbol] <= -2) & (vals_merged["%s.y" % symbol] >= -2) & (vals_merged["SPY.x"] >= 1)].head()
+
+    return df_events
+
+
+def find_events_h6(d_data, ndays=20):
+
+    df_close = d_data['close']
+    vals = bollinger_df(df_close, ndays)
+    vals_merged = pd.merge(vals, vals.shift(1), left_index=True, right_index=True)
+
+    # Creating an empty dataframe
+    df_events = copy.deepcopy(df_close)
+    df_events = df_events * np.NAN
+
+    for symbol in vals.columns:
+        df_events.ix[(vals_merged["%s.x" % symbol] < -2) & (vals_merged["%s.y" % symbol] >= -2) & (vals_merged["SPY.x"] >= 1.5), symbol] = 1
+
+    return df_events
+
+def find_events_h7(d_data, ndays=20):
+
+    df_close = d_data['close']
+    vals = bollinger_df(df_close, ndays)
+    vals_merged = pd.merge(vals, vals.shift(1), left_index=True, right_index=True)
+
+    # Creating an empty dataframe
+    df_events = copy.deepcopy(df_close)
+    df_events = df_events * np.NAN
+
+    for symbol in vals.columns:
+        df_events.ix[(vals_merged["%s.x" % symbol] < -2) & (vals_merged["%s.y" % symbol] >= -2) & (vals_merged["SPY.x"] >= 1.4), symbol] = 1
 
     return df_events
--- CompInvest/tests/quizes_w8_test.py
+++ CompInvest/tests/quizes_w8_test.py
@@ -2,7 +2,7 @@ __author__ = 'baio'
 
 import unittest
 
-from CompInvest.events_gens.bollinger_minus_2_spy_1 import  find_events
+from CompInvest.events_gens.bollinger_minus_2_spy_1 import  *
 from CompInvest import data_access
 import QSTK.qstkutil.DataAccess as da
 from CompInvest.strategy_maker import  write_strategy
@@ -12,6 +12,7 @@ from CompInvest.market_analyzer import analise_market
 
 class TestW8Functions(unittest.TestCase):
 
+    @unittest.skip("demonstrating skipping")
     def test_experiment(self):
         """
         http://wiki.quantsoftware.org/index.php?title=CompInvesti_Homework_7
@@ -27,6 +28,27 @@ class TestW8Functions(unittest.TestCase):
         """
         #build_market(100000, "data/orders_bollinger.csv", "data/market_sim_bollinger.csv")
         analise_market("data/market_sim_bollinger.csv", "$SPX", "data/market_sim_bollinger.png")
+
+    def test_experiment_h6(self):
+        dataobj = da.DataAccess('Yahoo')
+        symbols = dataobj.get_symbols_from_list('sp5002012')
+        symbols = symbols + ["SPY"]
+        keys = ["close"]
+        data = data_access.load("2008-01-01", "2009-12-31", 16, symbols, keys)
+        events = find_events_h6(data)
+        write_strategy("data/orders_bollinger_h6.csv", events, strategy)
+
+    @unittest.skip("demonstrating skipping")
+    def test_experiment_h7(self):
+        dataobj = da.DataAccess('Yahoo')
+        symbols = dataobj.get_symbols_from_list('sp5002012')
+        symbols = symbols + ["SPY"]
+        keys = ["close"]
+        data = data_access.load("2008-01-01", "2009-12-31", 16, symbols, keys)
+        events = find_events_h7(data)
+        write_strategy("data/orders_bollinger_h7.csv", events, strategy)
+        build_market(100000, "data/orders_bollinger_h7.csv", "data/market_sim_bollinger_h7.csv")
+        analise_market("data/market_sim_bollinger_h7.csv", "$SPX", "data/market_sim_bollinger_h7.png")
 
 if __name__ == '__main__':
     unittest.main()
